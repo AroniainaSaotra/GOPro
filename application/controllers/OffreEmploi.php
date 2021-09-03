ac7c0d262e14334390c77b935a5889763307532d
tHoxic01H <andriantsilavinaanthony@gmail.com>
2021-09-04 02:47:12 +0300

    offreEmploi1
    offreEmploi1

--- application/controllers/OffreEmploi.php
+++ application/controllers/OffreEmploi.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+/*metyyyyyyyyyyyyyyy vv ooooooooooo*/
 
 class OffreEmploi extends CI_Controller {
 
@@ -30,5 +31,55 @@ class OffreEmploi extends CI_Controller {
 		$data['listeFiliere']=$this->Branche->find("");
 		$this->load->view('filiere',$data);
 		*/
+	}
+
+	public function loginEntreprise()
+	{
+		$this->load->model('OffreEmploi');
+		$email = $this->input->get_post('email');
+		$mdp = $this->input->get_post('mdp');
+		$data = array();
+		$count = $this->OffreEmploi->loginEntreprise($email, $mdp);
+		if ($count != 0) {
+			$data = Array();
+			$data['vue'] = "#";// .php izay andehanany
+			$this->load->view('#',$data);// template apres login
+			$idEntreprise = $this->OffreEmploi->get_idEntreprise($email, $mdp);
+			$this->session->set_userdata('idEntreprise', $idEntreprise);
+		}
+		else{
+			$data['erreur'] = "Compte incorrecte !!";
+			$this->load->view('#', $data);// template apres erreur 
+		}
+
+	}
+
+	public function insertOffreEmploi()
+	{
+		$this->load->model('OffreEmploi');
+		$dateLimite = $this->input->get_post('dateLimite');
+		$ageMin = $this->input->get_post('ageMin');
+		$ageMax = $this->input->get_post('ageMax');
+		$dateInsertion = $this->input->get_post('dateInsertion');
+		$metier = $this->input->get_post('metier');
+		$idMetier =  $this->OffreEmploi->get_idMetier($metier);
+		$idEntreprise = $this->session->userdata('idEntreprise');
+		$this->OffreEmploi->insertOffreEmploi($idEntreprise,$dateLimite,$ageMin,$ageMax,$dateInsertion,$idMetier);
+		$data['offreEmploi'] = $this->OffreEmploi->get_offreEmploi();
+		$data['vue']="#";// .php izay andehanany
+		$this->load->view('#',$data);// template apres inseretions
+	}
+
+	public function supprimerOffreEmploi()
+	{
+		$this->load->model('OffreEmploi');
+		$data = array();
+		$this->OffreEmploi->deleteOffreEmploi($idProduit);
+		$data['offreEmploi'] = $this->OffreEmploi->get_offreEmploi();
+		$data['vue']="#";// .php izay andehanany
+		$this->load->view('#',$data);// template apres inseretions
+	}
+
+	
 	}
 }
--- application/models/OffreEmploi.php
+++ application/models/OffreEmploi.php
@@ -7,15 +7,19 @@
         private $dateLimite;
         private $ageMin;
         private $ageMax;
+        private $dateInsertion;
+        private $idMetier;
+
         
-        function __construct($id,$idEntreprise, $dateLimite, $ageMin, $ageMax) {
+        function __construct($id,$idEntreprise, $dateLimite, $ageMin, $ageMax,$dateInsertion,$idMetier) {
             $this->id = $id;
             $this->idEntreprise = $idEntreprise;
             $this->dateLimite = $dateLimite
             $this->ageMin = $ageMin;
             $this->ageMax = $ageMax;
+            $this->dateInsertion = $dateInsertion;
+            $this->idMetier = $idMetier;
         }
-
         function setId($id){
             $this->id = $id;
         }
@@ -31,7 +35,12 @@
         function setAgeMax($ageMax){
             $this->ageMax = $ageMax;
         }
-
+        function setDateInsertion($dateInsertion){
+            $this->dateInsertion=$dateInsertion;
+        }
+        function setIdMetier($idMetier){
+            $this->idMetier=$idMetier;
+        }
         function getId(){
             return $this->id;
         }
@@ -47,4 +56,77 @@
         function getAgeMax(){
             return $this->ageMax;
         }
+        function getDateInsertion(){
+            return $this->dateInsertion;
+        }
+        function getIdMetier(){
+            return $this->idMetier;
+        }
+        public function loginEntreprise($email, $mdp)
+        {
+            $motdepasse = sha1($mdp);
+            $sql1 = "SELECT count(*) as isa FROM Entreprise WHERE login = %s AND mdp = %s";
+            $sql1 = sprintf($sql1,$this->db->escape($email),$this->db->escape($motdepasse));
+            $query = $this->db->query($sql1);
+            $row = $query->row_array();
+            $count = $row['isa'];
+            return $count;
+        }
+
+        public function get_idEntreprise($email, $mdp)
+        {
+            $motdepasse = sha1($mdp);
+            $sql1 = "SELECT ID FROM Entreprise WHERE login = %s AND mdp = %s";
+            $sql1 = sprintf($sql1,$this->db->escape($email),$this->db->escape($motdepasse));
+            $query = $this->db->query($sql1);
+            $row = $query->row_array();
+            $idEntreprise = $row['ID'];
+            return $idEntreprisess;
+        }
+
+
+        public function insertOffreEmploi($idEntreprise,$dateLimite,$ageMin,$ageMax,$dateInsertion,$idMetier)
+        {
+            $sql = "INSERT INTO offreEmploi(idEntreprise,dateLimite,ageMin,ageMax,dateInsertion,idMetier)VALUES(%s,%s,%s,%s,%s,%s)";
+            $sql = sprintf($sql,$this->db->escape($idEntreprise),$this->db->escape($dateLimite),$this->db->escape($ageMin),$this->db->escape($ageMax),$this->db->escape($dateInsertion),$this->db->escape($idMetier));
+            $this->db->query($sql);
+        }
+
+        public function get_idMetier($nom)
+        {
+            $sql1 = "SELECT ID FROM metier WHERE nomMetier = %s ";
+            $sql1 = sprintf($sql1,$this->db->escape($email));
+            $query = $this->db->query($sql1);
+            $row = $query->row_array();
+            $idM = $row['ID'];
+            return $idM;
+        }
+
+        public function get_offreEmploi()
+        {
+            $query = $this->db->query('SELECT * FROM offreEmploi');
+            return $query->result_array() ;
+        }
+
+        public function deleteOffreEmploi($ID)
+        {
+            $sql = "DELETE  FROM offreEmploi WHERE ID = %s";
+            $sql = sprintf($sql,$ID);
+            $this->db->query($sql);
+        }
+        public function listOffre($idEntreprise)
+        {
+            $sql="SELECT * FROM offreEmploi WHERE idEntreprise=%d
+                  AND dateLimite>=SYSDATE() 
+                  AND ID not in (select idOffreEmploi from supressionOffreEmploi) 
+                 ";
+            $result = $this->db->query($requete)->row_array();
+            $offre=array();
+            foreach($result as $r){
+                $offre[] = new OffreEmploi($r['id'],$r['idEntreprise'], $r['dateLimite'],$r['ageMin'],$r['ageMax'],$r['dateInsertion'],$r['idMetier']);
+            }
+            return $offre;
+        }
+
+    }
     }
